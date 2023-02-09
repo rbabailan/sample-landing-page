5a2679b09ec3332a5e99dd69b9aa664f1278d692
Ronald D. Babailan <ronaldbabailan0026@gmail.com>
2023-02-09 18:53:03 +0400

    second commit

--- src/section/StudentTestimonials/StudentTestiminoals.js
+++ src/section/StudentTestimonials/StudentTestiminoals.js
@@ -1,16 +1,19 @@
 import CardTestimonial from "../../components/CardTestimonials/CardTestimonials";
 import style from "./StudentTestimonials.module.css";
+import FadeIn from "../../Animation/FadeIn";
 
 const StudentTestimonials = () => {
   return (
-    <div className={style.studentTestimonialsContainer}>
-      <h2>Students Testimonials</h2>
-      <div className={style.cardTestimonialsWrapper}>
-        <CardTestimonial name="Ronald" avatarImg="s" />
-        <CardTestimonial name="Ronald" avatarImg="s" />
-        <CardTestimonial name="Ronald" avatarImg="s" />
+    <FadeIn>
+      <div className={style.studentTestimonialsContainer}>
+        <h2>Students Testimonials</h2>
+        <div className={style.cardTestimonialsWrapper}>
+          <CardTestimonial name="Ronald" avatarImg="" />
+          <CardTestimonial name="Ronald" avatarImg="s" />
+          <CardTestimonial name="Ronald" avatarImg="s" />
+        </div>
       </div>
-    </div>
+    </FadeIn>
   );
 };
 
